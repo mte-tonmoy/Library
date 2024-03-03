376ebf98799b3587a1880600810d83a9d34494e1
Toufique Elahi Tonmoy <77458527+mte-tonmoy@users.noreply.github.com>
2024-03-03 19:34:05 +0600

    First-day

---
+++ src/Components/Home/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BoltIcon } from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      {/* Logo Section */}
+      <Link to="/" className="inline-flex items-center">
+        <BoltIcon className="h-6 w-6 text-blue-500" />
+        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+          nextPage
+        </span>
+      </Link>
+      {/* Nav Item section */}
+      <ul className="items-center hidden space-x-8 lg:flex font-semibold">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/book"
+            className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          >
+            Books
+          </NavLink>
+        </li>{" "}
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "text-blue-500" : "")}
+          >
+            about Us
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Components/Home/Home.jsx";
+import About from "./Components/About/About.jsx";
+import Book from "./Components/Book/Book.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path:'book',
+        element:<Book/>,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element:<About/>,
+  }
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
